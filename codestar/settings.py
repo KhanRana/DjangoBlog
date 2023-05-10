d6ea5e8986bf52579d8009b0d0688b91f93dacf8
KhanRana <47982799+KhanRana@users.noreply.github.com>
2023-05-10 13:10:23 +0100

    Alter status field and remove a bug

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['i-think-and-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['i-think-and-blog.herokuapp.com', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
